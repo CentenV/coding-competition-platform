b83a1916064eb04fe9b10ec78af792e178b93c38
vincent <vincent.nguyennf@gmail.com>
2024-03-20 18:53:30 -0600

    Prisma get data working

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
 
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
+async function test() {
+  const test: any = await prisma.competitor.findMany();
+  console.log(test);
+}
 
 export default function Home() {
+  test();
+
   return (
     <div>Coding Competition Platform</div>
   );
