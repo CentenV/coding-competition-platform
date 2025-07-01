dd94d3f162028c51f6cd161032e086fef798c763
vincent <vincent.nguyennf@gmail.com>
2025-06-30 19:29:44 -0600

    Attempt at adding global ESLint

--- app/server/eslint.config.mjs
+++ app/server/eslint.config.mjs
@@ -1,11 +1,3 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import { nodeESLintConfig } from "@repo/eslint/node-default"
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], ignores: ["build/**", "node_modules/**"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
-]);
+export default nodeESLintConfig;
