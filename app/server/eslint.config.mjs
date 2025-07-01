d9da0f35a2c003f8c464d2673d5230dd87677500
vincent <vincent.nguyennf@gmail.com>
2025-06-30 18:35:57 -0600

    Scaffolded all workspaces

--- app/server/eslint.config.mjs
+++ app/server/eslint.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], ignores: ["build/**", "node_modules/**"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
 ]);
