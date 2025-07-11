bb6ce6af55f40ab791eaeaeceb6d55626149534a
vincent <vincent.nguyennf@gmail.com>
2025-07-10 20:08:46 -0600

    Scaffolded

--- lib/service/src/service.ts
+++ lib/service/src/service.ts
@@ -1,1 +1,7 @@
-console.log("hello world")
+// Core Service Abstract //
+// Outlines the base behavior that is shared amongst all other services
+
+class CodeCompPlatService {
+  constructor() {
+  }
+}
