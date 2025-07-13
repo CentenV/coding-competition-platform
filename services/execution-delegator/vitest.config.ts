43afd75c4644d98c48c69ade203359eaa898956f
Vincent Nguyen <58229529+CentenV@users.noreply.github.com>
2025-07-13 17:12:59 -0600

    Added Vitest to repo (#14)

---
+++ services/execution-delegator/vitest.config.ts
@@ -0,0 +1,3 @@
+import { config as viteConfig } from "@codecompplat/config-vitest/backend";
+
+export default viteConfig;
---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    projects: [
+      "app/*/vitest.config.ts",
+      "services/*/vitest.config.ts"
+    ],
+    coverage: {
+      exclude: ["**/*.js"],
+      provider: "istanbul",
+      reporter: ["text", "json", "html"],
+      reportsDirectory: "./.coverage"
+    }
+  }
+})
