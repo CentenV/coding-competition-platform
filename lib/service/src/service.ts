d8dc457a17964afd76e34692e84042d41e863c77
vincent <vincent.nguyennf@gmail.com>
2025-07-15 23:15:21 -0600

    Scaffolded out abstract service class code

--- lib/service/src/service.ts
+++ lib/service/src/service.ts
@@ -1,7 +1,30 @@
 // Core Service Abstract //
 // Outlines the base behavior that is shared amongst all other services
 
-class CodeCompPlatService {
-  constructor() {
+export abstract class CodeCompPlatService {
+  private name: string;
+
+  /*
+   * Creates a new service
+   * @param {string} name 
+   * */
+  constructor(name: string) {
+    this.name = name;
   }
+
+  /*
+   * Start the service. Invokes this.cycle() infinitely until SIG
+   * */
+  public start(): void {
+    // Register signals
+    process.on("SIGINT", this.shutdown)
+    process.on("SIGQUIT", this.shutdown)
+    process.on("SIGTERM", this.shutdown)
+    // Invoke cycle method
+    while (true) { this.cycle() }
+  }
+
+  public abstract shutdown(): void;
+
+  public abstract cycle(): void;
 }
